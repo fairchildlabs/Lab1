d21da5f2b65112a41ac05577fe01d7ab1afd16c2
Brandon Awbrey <Brandon.Awbrey@microchip.com>
2024-09-12 08:05:30 -0500

    Lab1 update

--- code/scootd.c
+++ code/scootd.c
@@ -6,15 +6,29 @@
 
 int main(int argc, char **argv)
 {
-
-	scoot_device scootDevice;
+	unsigned int old_state = 0;
+	scoot_device *pScootDevice;
 
 	printf("scootd - Lab1\n");
 
 	scootDevice.state = 0;
 
-	scootd_util_open_shared_memory("scootd_shared.mem");
-	
+	pScootDevice = scootd_util_open_shared_memory("scootd_shared.mem");
+
+	if(pScootDevice)
+	{
+		while(1)
+		{
+			if(old_state != pScootDevice->state)
+			{
+				printf("State Change old_state = %d new_state = %d\n", old_state, pScootDevice->state);
+				old_state = pScootDevice->state;
+				sleep(1);
+			}
+
+		}
+
+	}
 
 
 	return 0;
